d74b3eeb385d8c8734b05c69681d10fee90b4e62
root <bleneskindr@gmail.com>
2023-03-16 06:16:06 -0700

    my commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{ char a;
+  int b;
+  long c;
+  long long int d;
+  float e;
+  int main(void)
+{
+  printf("size of a char: ",sizeof(a));
+  printf("size of an int: ",sizeof(b));
+  printf("size of a long int: ",sizeof(c));
+  printf("size of a long long int: ",sizeof(d));
+  printf("size of a float: ",sizeof(e));
+  return(0);
+}
+  
