e3dadedc39f868133e69a2afed7fbf56692f9e50
root <bleneskindr@gmail.com>
2023-05-04 01:36:49 -0700

    my commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to the binary number string
+ *
+ * Return: the converted number, or 0 if there is one or more
+ * chars in the string b that is not 0 or 1, or if b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		result = result << 1;
+		if (b[i] == '1')
+			result = result | 1;
+	}
+
+	return (result);
+}
